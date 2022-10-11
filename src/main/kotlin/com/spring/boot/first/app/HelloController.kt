5516e4a852b8b1055762dc71aac9a0df4e7c8d25
Takumaron <tennis.takumax@gmail.com>
2022-10-11 23:20:06 +0900

    first commit

---
+++ src/main/kotlin/com/spring/boot/first/app/HelloController.kt
@@ -0,0 +1,14 @@
+package com.spring.boot.first.app
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HelloController {
+    @GetMapping("/")
+    fun index(model: Model): String {
+        model.addAttribute("message", "hello, world")
+        return "index"
+    }
+}
